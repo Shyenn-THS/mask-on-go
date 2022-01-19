10c8aaaab96f25a63470dff481806aea80c5ecfa
ShivangM <shivangmishra0824@gmail.com>
2022-01-20 01:06:45 +0530

    Python Coversion

---
+++ ANPR/Save.py
@@ -0,0 +1,15 @@
+import csv
+import uuid
+from OCR import *
+
+'{}.jpg'.format(uuid.uuid1())
+def save_results(text, region, csv_filename, folder_path):
+    img_name = '{}.jpg'.format(uuid.uuid1())
+    
+    cv2.imwrite(os.path.join(folder_path, img_name), region)
+    
+    with open(csv_filename, mode='a', newline='') as f:
+        csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([img_name, text])
+
+save_results(text, region, 'detection_results.csv', 'detectionResults')
