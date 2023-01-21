987dc6e86ac40eca1a65c397c875013ad0cae139
ShivangM <shivangmishra0824@gmail.com>
2023-01-21 12:43:45 +0530

    Rearrange

---
+++ Load_Model.py
@@ -0,0 +1,31 @@
+import os
+import tensorflow as tf
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as viz_utils
+from object_detection.builders import model_builder
+from object_detection.utils import config_util
+from Path_Setup import *
+
+# Prevent GPU complete consumption
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+    try: 
+        tf.config.experimental.set_virtual_device_configuration(
+            gpus[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=5120)])
+    except RunTimeError as e:
+        print(e)
+
+# Load pipeline config and build a detection model
+configs = config_util.get_configs_from_pipeline_file(files['PIPELINE_CONFIG'])
+detection_model = model_builder.build(model_config=configs['model'], is_training=False)
+
+# Restore checkpoint
+ckpt = tf.compat.v2.train.Checkpoint(model=detection_model)
+ckpt.restore(os.path.join(paths['CHECKPOINT_PATH'], 'ckpt-11')).expect_partial()
+
+@tf.function
+def detect_fn(image):
+    image, shapes = detection_model.preprocess(image)
+    prediction_dict = detection_model.predict(image, shapes)
+    detections = detection_model.postprocess(prediction_dict, shapes)
+    return detections
---
+++ Save.py
@@ -0,0 +1,27 @@
+import uuid
+from OCR import *
+from dotenv import load_dotenv
+load_dotenv()
+
+def get_database():
+    from pymongo import MongoClient
+
+    # Provide the mongodb atlas url to connect python to mongodb using pymongo
+    CONNECTION_STRING = os.getenv('DB_URI')
+
+    # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
+    client = MongoClient(CONNECTION_STRING)
+
+    # Create the database for our example (we will use the same database throughout the tutorial
+    return client['NPR']
+
+dbname = get_database()
+number_plates = dbname["NumberPlates"]
+
+'{}.jpg'.format(uuid.uuid1())
+def save_results(text, region, folder_path):
+    img_name = '{}.jpg'.format(uuid.uuid1())
+    
+    cv2.imwrite(os.path.join(folder_path, img_name), region)
+    data = {"img_name": img_name, "text": text[0]}
+    number_plates.insert_one(data)
